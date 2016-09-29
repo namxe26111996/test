0280eb5f129c281312cea89ff048de637da14ceb
Nguyễn Văn Hiến <vanhien96@gmail.com>
2016-09-29 15:46:03 +0700

    Hien

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(65, 72);
+            this.button1.Location = new System.Drawing.Point(74, 57);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(132, 61);
             this.button1.TabIndex = 0;
